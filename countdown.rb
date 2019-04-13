612af12558dfb802f0c0eabe0dcbf39ea7130ccf
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 18:47:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,2 +1,7 @@
-def countdown
-end 
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
